13d925f20931f36ff01209c0f217861ebeb22aac
Николай <nickneustroev2@gmail.com>
2026-06-13 19:54:50 +0700

    123

--- app/components/Charts/ChartWrapper.vue
+++ app/components/Charts/ChartWrapper.vue
@@ -12,11 +12,11 @@ withDefaults(
 
 <template>
   <div
-    class="mx-auto max-w-3xl space-y-6 rounded-lg"
+    class="mx-auto max-w-3xl space-y-2 rounded-lg pt-12"
     :class="showTitle ? 'p-6' : ''"
   >
     <div class="flex items-center justify-between">
-      <h3 class="text-lg font-semibold">
+      <h3 class="text-2xl font-semibold">
         {{ title }}
       </h3>
     </div>
